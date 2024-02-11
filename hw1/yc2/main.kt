7d331e4496d4b0df485ed3b53103bd1dc8c135c3
EgorovGA <egorov.ga@minc.tech>
2024-02-11 20:20:00 +0300

    hw1 yc2 - rewrite as hashset

--- hw1/yc2/main.kt
+++ hw1/yc2/main.kt
@@ -1,17 +1,25 @@
 fun fibMod(n: Long, m: Long): Long {
-    var cache: List<Long> = listOf(0, 1)
-    var fibPrev: Long = 1
-    var fibCur: Long = 1
-    while (fibPrev != cache[0] || fibCur != cache[1]) {
-        cache += fibCur
-        var fibTemp = fibPrev
-        fibPrev = fibCur
-        fibCur = (fibTemp + fibCur) % m
+    val MAXMOD: Long = 1000000
+    var cache: List<Long> = listOf(0 + 1 * MAXMOD)
+    var cacheSet: HashSet<Long> = hashSetOf(0 + 1 * MAXMOD)
+    var fibPair: Long = 1 + 1 * MAXMOD
+    while (!cacheSet.contains(fibPair)) {
+        var fib1: Long = fibPair % MAXMOD
+        var fib2: Long = fibPair / MAXMOD
+        cache += fibPair
+        cacheSet += fibPair
+        fibPair = fib2 + ((fib1 + fib2) % m) * MAXMOD
     }
-    return cache[(n % (cache.size - 1)).toInt()]
+    var cycleStart = cache.indexOf(fibPair)
+    var cycleSize = cache.size - cycleStart;
+    var index = (cycleStart + (n - cycleStart) % cycleSize).toInt()
+    return cache[index] % MAXMOD
 }
 
 fun main(args: Array<String>) {
+    if (fibMod(10, 2) != 1L) return;
+    if (fibMod(10, 3) != 1L) return;
+    if (fibMod(10, 4) != 3L) return;
     var (n, m) = readln().split(' ')
     println(fibMod(n.toLong(), m.toLong()))
 }
