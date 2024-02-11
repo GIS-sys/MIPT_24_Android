f2900ff6db8e268c215e382cd41b7220efa81709
EgorovGA <egorov.ga@minc.tech>
2024-02-11 19:15:49 +0300

    remove ;

--- hw1/yc2/main.kt
+++ hw1/yc2/main.kt
@@ -1,14 +1,14 @@
 fun fibMod(n: Long, m: Long): Long {
-    var cache: List<Long> = listOf(0, 1);
-    var fibPrev: Long = 1;
-    var fibCur: Long = 1;
+    var cache: List<Long> = listOf(0, 1)
+    var fibPrev: Long = 1
+    var fibCur: Long = 1
     while (fibPrev != cache[0] || fibCur != cache[1]) {
-        cache += fibCur;
-        var fibTemp = fibPrev;
-        fibPrev = fibCur;
-        fibCur = (fibTemp + fibCur) % m;
+        cache += fibCur
+        var fibTemp = fibPrev
+        fibPrev = fibCur
+        fibCur = (fibTemp + fibCur) % m
     }
-    return cache[(n % (cache.size - 1)).toInt()];
+    return cache[(n % (cache.size - 1)).toInt()]
 }
 
 fun main(args: Array<String>) {
